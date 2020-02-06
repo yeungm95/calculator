c37a47ec3b818702612d83348e5403036665cedf
Michael Yeung <yeungm95@gmail.com>
2020-02-06 05:45:21 -0800

    add empty README.md file and calulator js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
 
-alert(add(number1, number2));
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+var calculatorResult = 
 
