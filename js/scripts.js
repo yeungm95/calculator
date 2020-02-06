9a5bf5e32dcb13dcf41e28a8dc77034f34891aca
Michael Yeung <yeungm95@gmail.com>
2020-02-06 07:00:32 -0800

    write functions that convert temperatures from F to C or C to F

--- js/scripts.js
+++ js/scripts.js
@@ -20,11 +20,29 @@ var number2 = parseInt(prompt("Enter another number:"));
 var calculatorResult
 alert(calculatorResult);
 
+
 var bmi = function(weight, height) {
 	return (weight/(height**2)) * 703;
-} 
+}; 
 var weight = parseInt(prompt("What is your weight in lbs?"));
 var height = parseInt(prompt("What is your height in inches?"));
 
 var bmiResult = bmi(weight, height).toPrecision(3);
 alert(bmiResult)
+
+
+var toCelcius = function(temp) {
+  return (temp - 32) / 1.8;
+};
+
+var toFahrenheit = function(temp) {
+  return (temp * 1.8) + 32;
+};
+
+var temp = parseInt(prompt("What is the temperature in Fahrenheit?"));
+var temp = parseInt(prompt("What is the temperature in Celcius?"));
+
+// alert(toFahrenheit(temp))
+// alert(toCelcius(temp))
+
+
