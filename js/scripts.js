6b10a5153c9b5de62d53c727fadf2d73e3111653
Student <example@example.com>
2020-02-13 18:49:00 -0800

    add front-end logic to calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 
 // Calculator
+
+//Back-end logic
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -15,32 +17,44 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+//Front-end logic
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
 
-var calculatorResult
-alert(calculatorResult);
 
-// BMI
-var bmi = function(weight, height) {
-	return (weight/(height**2)) * 703;
-}; 
-var weight = parseInt(prompt("What is your weight in lbs?"));
-var height = parseInt(prompt("What is your height in inches?"));
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
 
-var bmiResult = bmi(weight, height).toPrecision(3);
-alert(bmiResult)
+// var calculatorResult
+// alert(calculatorResult);
 
-// Temperature
-var toCelcius = function(temp) {
-  return (temp - 32) / 1.8;
-};
+// // BMI
+// var bmi = function(weight, height) {
+// 	return (weight/(height**2)) * 703;
+// }; 
+// var weight = parseInt(prompt("What is your weight in lbs?"));
+// var height = parseInt(prompt("What is your height in inches?"));
 
-var toFahrenheit = function(temp) {
-  return (temp * 1.8) + 32;
-};
+// var bmiResult = bmi(weight, height).toPrecision(3);
+// alert(bmiResult)
 
-var temp = parseInt(prompt("What is the temperature in Fahrenheit?")); // alert(toCelcius(temp))
-var temp = parseInt(prompt("What is the temperature in Celcius?")); // alert(toFahrenheit(temp))
+// // Temperature
+// var toCelcius = function(temp) {
+//   return (temp - 32) / 1.8;
+// };
+
+// var toFahrenheit = function(temp) {
+//   return (temp * 1.8) + 32;
+// };
+
+// var temp = parseInt(prompt("What is the temperature in Fahrenheit?")); // alert(toCelcius(temp))
+// var temp = parseInt(prompt("What is the temperature in Celcius?")); // alert(toFahrenheit(temp))
 
 
