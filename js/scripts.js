2d74991b4942a3ae707b39bd360e195909de167a
Student <example@example.com>
2020-02-13 19:16:00 -0800

    add interface and back-end logic for multiplication and division

--- js/scripts.js
+++ js/scripts.js
@@ -31,8 +31,23 @@ $(document).ready(function() {
     event.preventDefault();
     var number1 = parseInt($("#subtract1").val());
     var number2 = parseInt($("#subtract2").val());
-    debugger;
     var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
     $("#output").text(result);
   });
 });
